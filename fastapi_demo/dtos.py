227978c36a4599df0b19f38d19f098994b19b955
Alex Jantunen <alex.jantunen@eficode.com>
2024-10-11 08:59:58 +0300

    GenAI created implementation of the task Impl: Backend Implementation Specification for 'Create and Persist Author Information for Books'

--- fastapi_demo/dtos.py
+++ fastapi_demo/dtos.py
@@ -1,11 +1,10 @@
 from pydantic import BaseModel
 from typing import Optional
 
+class AuthorCreate(BaseModel):
+    name: str
+    biography: str
+    date_of_birth: str
 
-class BookCreate(BaseModel):
-    title: str
-    author: str
-    pages: int
-
-class BookInfo(BookCreate):
+class AuthorInfo(AuthorCreate):
     id: Optional[int] = None
