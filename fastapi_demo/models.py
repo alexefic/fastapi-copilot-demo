ce6ada2655dd6fc1deb3749615f2f6e6b0c62227
Kalle Mäkelä <kalle.makela@eficode.com>
2024-11-14 11:52:03 +0200

    GenAI created implementation of the task Impl: Backend Implementation Specification for 'Search for Children's Book Category'

--- fastapi_demo/models.py
+++ fastapi_demo/models.py
@@ -1,11 +1,17 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String
 
-
 class Book(Base):
     __tablename__ = "books"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     author = Column(String, index=True)
     pages = Column(Integer)
+    category = Column(String, index=True)  # New field
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    subscription_status = Column(String, default="Active")
